02d1b69628f82e6139c52dab6b87e6817fcedf35
waqas saleemm <waqas.mba86@gmail.com>
2019-05-17 17:29:29 -0600

    additions on 18.05.2019

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -16,11 +16,10 @@ class Introduction extends Component {
           <br/><br/><br/><br/><br/><br/>
         <h2 className='heading2'>Company Profile & Infrastructure</h2>
         <p className='p1'>XYZ Textile, one of the leading textile industries having a history of decades, is providing premium quality textiles. It has accomplished a far-fetched by following a strong vow to the clients by giving them top quality products and services. Strong sense to adopt latest technical innovation, having a glance over worldwide vogue in textile industry and giving full fledged dedicated services to the clients is the dynamic key of “XYZ Textile” vertical growth. Company produces complete variety of textiles ranging from grey fabric to highly profiled made-ups. Its export zone has vast borders from Europe, North & South America, Russia & CIS States to Scandinavian countries. Company has achieved the turnover of USD $70 Million per annum since years. “We are the Safe Supplier” signifies Company is transforming this creed into certainty through excellent, skilled and dedicated, 2500+ employees who assure the best availability of products & services in the hands of Clients.</p>
+      
         <div className='container'>
         <div className='row'>
-
-        <div className='col-4'> <img src={infra3} alt='pic here' width='250px' height='180px'/>  <br/><br/>  <img src={infra4} alt='pic here' width='250px' height='180px'/> </div>           <div className='col-4'> <img src={infra1} alt='pic here' width='250px' height='180px'/>  <br/> <br/>  <img src={infra5} alt='pic here' width='250px' height='180px'/> </div>     <div className='col-4'> <img src={infra2} alt='pic here' width='250px' height='180px'/>  <br/> <br/>   <img src={infra6} alt='pic here' width='250px' height='180px'/></div>
-        
+       <div className='col-4'> <img src={infra3} alt='pic here' width='250px' height='180px'/>  <br/><br/>  <img src={infra4} alt='pic here' width='250px' height='180px'/> </div>           <div className='col-4'> <img src={infra1} alt='pic here' width='250px' height='180px'/>  <br/> <br/>  <img src={infra5} alt='pic here' width='250px' height='180px'/> </div>     <div className='col-4'> <img src={infra2} alt='pic here' width='250px' height='180px'/>  <br/> <br/>   <img src={infra6} alt='pic here' width='250px' height='180px'/></div>
         </div>
         </div>
 
@@ -28,7 +27,8 @@ class Introduction extends Component {
         <p className='p1'>We maintained and continue to improve a high level of quality in everything we produce by following specific standards. This start from the very initial stage of sourcing raw material to the final stage of finished product.</p>
         <h2 className='heading2'>Team Work</h2>
         <p className='p1'> Being a world class Textile house, XYZ Textile upholds that an organization’s success is hinged not only on a great product line-up also on how it selects, trains, and coaches people to undertake higher responsibilities; to nurture talent, and to build leaders of tomorrow. Our export marketing team work passionately to fully entertain all the queries, complaints and questions. We make sure the customer’s satisfaction by provision of services before, during and after purchase </p>
-        
+        <div className='div3'> <br/> <b>Head Office:</b> <p>Kashmir Road, Faisalabad, Pakistan. </p> <b>Mills: </b> <p>3-Kilo meter, jhumra Road, khurrianwala, Faisalabad <br/> <b>Contact:</b> 92346xxxxxxxx <br/> <b>E-mail:</b> xxxyyz@xyz.com <br/> <br/></p> </div>
+
           </div>
     );
   }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../App.css'
-
+import prod1 from '../images/prod1.jpg'
+import prod2 from '../images/prod2.jpg'
+import prod3 from '../images/prod3.jpeg'
+import prod4 from '../images/prod4.jpeg'
+import prod5 from '../images/prod5.jpeg'
+import prod6 from '../images/prod6.jpeg'
+import prod7 from '../images/prod7.jpeg'
+import prod8 from '../images/prod8.jpeg'
+import prod9 from '../images/prod9.jpeg'
 
 
 class Product extends Component {
   render() {
     return (
       <div>
-          <br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/>
         
-        <h1>Some Example Text - Product</h1>
-        
+        <h2 className='heading2'>Company Products</h2>
+        <p className='p1'>We maintained and continue to improve a high level of quality in everything we produce by following specific standards. This start from the very initial stage of sourcing raw material to the final stage of finished product.</p>
+        <h3 className='heading2'>Made Ups</h3>
+
+        <div className='container'>
+        <div className='row'>
+       <div className='col-4'> <img src={prod1} alt='pic here' width='250px' height='180px'/>  <br/><br/>  <img src={prod2} alt='pic here' width='250px' height='180px'/>   <br/><br/>  <img src={prod3} alt='pic here' width='250px' height='180px'/>  </div>           <div className='col-4'> <img src={prod4} alt='pic here' width='250px' height='180px'/>  <br/><br/>  <img src={prod5} alt='pic here' width='250px' height='180px'/>   <br/><br/>  <img src={prod6} alt='pic here' width='250px' height='180px'/> </div>     <div className='col-4'> <img src={prod7} alt='pic here' width='250px' height='180px'/>  <br/><br/>  <img src={prod8} alt='pic here' width='250px' height='180px'/>   <br/><br/>  <img src={prod9} alt='pic here' width='250px' height='180px'/></div>
+        </div>
+        </div>
+
+
           </div>
     );
   }
