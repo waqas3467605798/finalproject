20452c51caccce247ffc44d6b7ccc23ccd2920f1
waqas saleemm <waqas.mba86@gmail.com>
2019-05-19 02:18:52 -0600

    2nd addition on 19.05.2019

--- src/components/Career.js
+++ src/components/Career.js
@@ -29,21 +29,29 @@ class Career extends Component {
           <tr>  <td>Your Name</td>  <td> <input type='text' placeholder='Name as per CNIC'/> </td>  </tr>
           <tr>  <td>Father Name</td>  <td> <input type='text' placeholder='Father Name'/> </td>  </tr>
           <tr>  <td>CNIC Number</td>  <td> <input type='text' placeholder='xxxxx-xxxxxxx-x'/> </td>  </tr>
-          <tr>  <td>Domicile</td>  <td> <input type='text' placeholder='Domicile'/> </td>  </tr>
+          <tr>  <td>Domicile</td>  <td> <input type='text' placeholder='Province/City'/> </td>  </tr>
+          <tr>  <td>Date of Birth</td>  <td> <input type='text' placeholder='dd/mm/yyyy'/> </td>  </tr>
+          <tr>  <td>Mobile Number</td>  <td> <input type='text' placeholder='923xxxxxxxxx'/> </td>  </tr>
+          <tr>  <td>Mailing Address</td>  <td> <input type='text' placeholder='Home Address'/> </td>  </tr>
           <tr>  <td>Gender</td>  <td> <input type='radio' name='gender' value='male'/>Male <input type='radio' name='gender' value='female'/>Female </td>  </tr>
           <tr>  <td>Qualification</td>  <td> <input type='text' placeholder='Latest Degree Name'/> </td>  </tr>
           <tr>  <td>Marks Obtained</td>  <td> <input type='text' placeholder='Marks Obtained'/> </td>  </tr>
           <tr>  <td>Percentage/CGP</td>  <td> <input type='text' placeholder='%'/> </td>  </tr>
           <tr>  <td>Post Applied For</td>  <td> <input type='text' placeholder='Department Name'/> </td>  </tr>
-          <tr>  <td>Experience</td>  <td> <input type='text' placeholder='Experience in Years'/> </td>  </tr>
-          <tr>  <td></td>  <td>  <button onClick={this.formHandler}>Submit</button></td>  </tr>
+          <tr>  <td>Experience</td>  <td> <input type='text' placeholder='Experience in Years'/> </td>  </tr> <br/>
+          <tr>  <td></td>  <td>  <button onClick={this.formHandler} className='btn1'>Submit</button></td>  </tr>
          </table>
 
        </form>
       </div>
       <div className='col-2'></div>
       </div>
-       </div>      
+       </div>    
+       <br/> <br/>
+       <h1 className='heading2'>OR</h1>  
+       <h5 className='heading2'>E-mail your C.V at </h5>  
+       <h6 className='heading2'>hr@xyz.com </h6>
+
 <br/> <br/>
 
 <div className='div3'> <br/> <b>Head Office:</b> <p>Kashmir Road, Faisalabad, Pakistan. </p> <b>Mills: </b> <p>3-Kilo meter, jhumra Road, khurrianwala, Faisalabad <br/> <b>Contact:</b> 92346xxxxxxxx <br/> <b>E-mail:</b> xxxyyz@xyz.com <br/> <br/></p> </div>
